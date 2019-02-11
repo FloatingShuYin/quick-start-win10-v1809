f21ccd3852b356d92339eebde96f623322cbab47
FloatingShuYin <floatshuyin@gmail.com>
2019-02-11 16:34:32 +0800

    fix

--- ps1/installBaseEnv.ps1
+++ ps1/installBaseEnv.ps1
@@ -1,17 +1,10 @@
 Write-Host "install base env" -Foreground "Cyan"
 # Multi-connection downloads with aria2
-$job0 = Start-Job -ScriptBlock { scoop install aria2 }
-scoop install sudo
-Wait-Job $job0
-$result0 = Receive-Job $job0
-$result0
-Remove-Job -Job $job0
-$job1 = Start-Job -ScriptBlock { sudo scoop install 7zip --global }
-Wait-Job $job1
-$result1 = Receive-Job $job1
-$result
-Remove-Job -Job $job1
-Get-Job
+Start-Job -ScriptBlock { scoop install aria2 }
+Start-Job -ScriptBlock { scoop install sudo }
+Get-Job | Wait-Job | Receive-Job
+
+sudo scoop install 7zip --global
 # install git
 sudo scoop install git --global
 Write-Host "install base env Done!" -Foreground "Cyan"
