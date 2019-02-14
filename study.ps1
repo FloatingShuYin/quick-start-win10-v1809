c362c865cf4ebe36adcb9cef7a4d7a9f4f8b127b
FloatingShuYin <floatshuyin@gmail.com>
2019-02-14 20:36:47 +0800

    del

--- study.ps1
+++
@@ -1,5 +0,0 @@
-# $json = (Get-Content -encoding UTF8 O:\GitWorkSpace\quick-start-win10-v1809\config.json) | ConvertFrom-Json
-# $json | ConvertTo-Json | Out-File 'my.json'
-
-$path = "O:\GITWOR~1\QUICK-~1\assert\reg\utf-8.reg"
-start $path
