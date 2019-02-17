ea4147fcebed2e84630699d4bb1ff3b6e68462f6
FloatingShuYin <floatshuyin@gmail.com>
2019-02-17 13:26:22 +0800

    add window-build-tool

--- mod/aboutNode.ps1
+++ mod/aboutNode.ps1
@@ -5,4 +5,5 @@ sleep -mill 1000
 node -v | Out-String -Stream
 npm -v | Out-String -Stream
 npm i yarn -g | Out-String -Stream
+sudo Start-Process -FilePath "cmd.exe" -ArgumentList "/k","yarn global add windows-build-tools"
 Write-Host "install node Done!" -Foreground "Cyan"
